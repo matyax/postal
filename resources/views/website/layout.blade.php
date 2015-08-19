6002c5d8721e5a7eab51a9408b0544d9db0a1469
Matias Chomicki <matyax@gmail.com>
2015-08-19 19:39:20 -0300

    Add meta info

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Postal Urbana</title>
-        <meta name="description" content="">
+        <meta name="keywords" content="postal, urbana, diseño, web, marketing, redes, sociales, mendoza">
+        <meta name="description" content="Postal Urbana - Diseño gráfico, web, marketing digital y redes sociales. Mendoza, Argentina">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href='http://fonts.googleapis.com/css?family=Sanchez:400italic,400' rel='stylesheet' type='text/css'>
         <!-- Place favicon.ico in the root directory -->
